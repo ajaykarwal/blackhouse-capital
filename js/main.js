ba02fbcb91c2ef38fa4840eace119aba38436730
Ajay Karwal <ajaykarwal@gmail.com>
2018-02-13 23:29:22 +0000

    shuffle letters

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 ---
 ---
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Mobile navigation toggle button
-    $('.nav-toggle').click(function() {
+    $('.nav-toggle').click(function () {
         $(this).toggleClass('open');
         $('body').toggleClass('nav-open');
         $('.nav-container').slideToggle(200);
     });
+
+    var blackhouse = $('.blackhouse');
+    var capital = $('.capital');
+
+    blackhouse.hide();
+    setTimeout(function () {
+        blackhouse.show();
+        blackhouse.shuffleLetters({
+            "step": 8,			// How many times should the letters be changed
+            "fps": 25
+        });
+    }, 500);
+
+    capital.hide();
+    setTimeout(function () {
+        capital.show();
+        capital.shuffleLetters({
+            "step": 8,			// How many times should the letters be changed
+            "fps": 25
+        });
+    }, 500);
 });
